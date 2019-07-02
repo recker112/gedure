e82bd041679c4163602d63fad68812efbafe1e3f
recker112 <joseortiz112001@gmail.com>
2019-07-02 17:44:29 -0400

    Modificación de los nombres de las paginas mediante el head.php

--- head.php
+++ head.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="es">
 	<head>
-		<title>La Candelaria</title>
+		<title>UEP APEP "La Candelaria"</title>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
