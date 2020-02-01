e817732413c0dfa6e6ebb60acf7316529c689d2f
recker112 <joseortiz112001@gmail.com>
2020-02-01 17:28:12 +0000

    Intento de Noticias por API

--- frontend/src/components/News/components/ListNoticias.js
+++ frontend/src/components/News/components/ListNoticias.js
@@ -1,28 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Paper } from '@material-ui/core';
+import { consultAjax } from '../../reutilizar/ajaxConsult';
 
 export function ListNoticias() {
+  const [DataGet, setDataGet] = useState({
+    name: "Luis",
+    title: "title REACTIVO",
+    content: "Contenido papÁ.",
+    imgList: {
+      img1: {
+        title: 'Imagen1'
+      },
+      img2: {
+        title: 'Imagen2'
+      },
+      lenth: 2
+    }
+  });
+
   return (
   <article className="BoxNoticias">
+    {DataGet.name !== ''}{
+      <Noticia options={DataGet} />
+    }
+  </article>
+  );
+}
+
+function Noticia(props) {
+  const { name, content, title, imgList } = props.options;
+
+  return (
     <Paper variant="outlined">
       <section className="Noticia">
         <div className="NHead">
           <span className="NHeadImg">Logo</span>
-          <span className="NHeadName">Nombre</span>
+          <span className="NHeadName">{name}</span>
         </div>
         <hr />
         <div className="NContent">
-          <span className="NContentTitle">Titulo largo y chavista</span>
-          <p className="NContentP">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <span className="NContentTitle">{title}</span>
+          <p className="NContentP">{content}</p>
         </div>
         <footer >
-          <span className="imagen">Imagenes</span>
-          <span className="imagen">Imagenes</span>
-          <span className="Imagen">Imagenes</span>
-          <span className="Imagen">Imagenes</span>
+          <span>Imagenes</span>
+          <span>Imagenes</span>
+          <span>Imagenes</span>
+          <span>Imagenes</span>
         </footer>
       </section>
     </Paper>
-  </article>
-  );
+  )
 }
