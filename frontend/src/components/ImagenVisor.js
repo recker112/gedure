c5ea38314fcddbe20403dec39f56834380973424
IDEO-Recker <joseortiz112001@gmail.com>
2020-02-16 23:49:29 +0000

    Skeleton structure News
    -Se implementóel diseÃo del skeleton
    en "NEWS".
    -Se eliminaron los archivos anuncio.js
    y noticia.js, aÃadiendolos adentro de
    los archivos List de cada uno.

--- frontend/src/components/ImagenVisor.js
+++ frontend/src/components/ImagenVisor.js
@@ -1,15 +1,25 @@
 import React from 'react';
-export function ImagenVisor(props) {
-  let imagenes = props.options.map((element, i) => {
-    if (i === 3) {
-      return (<span key={i}>Hay mas</span>);
-    }
-    else if (i < 4) {
-      return (<span key={i}>{element.title}</span>);
-    }
-    else {
-      return (<span key={i} style={{ display: "none" }}>{element.title}</span>);
-    }
-  });
-  return (<footer>{imagenes}</footer>);
+
+//Material-UI
+import { Skeleton } from '@material-ui/lab';
+
+export function ImagenVisor({options}) {
+	
+	if (Array.isArray(options)) {
+		const imagenes = options.map((element, i) => {
+			if (i === 3) {
+				return (<span key={i}>Hay mas</span>);
+			}
+			else if (i < 4) {
+				return (<span key={i}>{element.title}</span>);
+			}
+			else {
+				return (<span key={i} style={{ display: "none" }}>{element.title}</span>);
+			}
+		});
+		return (<footer>{imagenes}</footer>);
+	}
+	
+	let SkeletonImg = [1,2,3,4].map(()=> <Skeleton variant="rect" width={70} height={60} />)
+	return (<footer>{SkeletonImg}</footer>);
 }
--- frontend/src/views/news/ListNoticias.js
+++ frontend/src/views/news/ListNoticias.js
@@ -2,55 +2,108 @@
 import React, { useEffect } from 'react';
 
 //Componentes
-import Noticia from './../../components/Noticia';
+import { ImagenVisor } from '../../components/ImagenVisor';
 
 //Redux
 import { connect } from 'react-redux';
 import { updateNewsNoticias } from '../../actions/news/updateNews';
+
+//Material-UI
+import { Paper, Grow } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
 
-export function ListNoticias({list, updateNewsNoticias}) {
-  useEffect(() => {
-    const fetchData = async ()=>{
-      const res = await fetch('https://my-json-server.typicode.com/recker112/candelariaweb/noticias')
-      .then(response => response.json())
-      .then(json => json)
+export function ListNoticias({ list, updateNewsNoticias }) {
+	useEffect(
+		() => {
+			const fetchData = async () => {
+				const res = await fetch(
+					'https://my-json-server.typicode.com/recker112/candelariaweb/noticias'
+				)
+					.then(response => response.json())
+					.then(json => json);
 
-      if (res !== 'no_connect'){
-      }
+				if (res !== 'no_connect') {
+				}
 
-      updateNewsNoticias(res);
-    }
+				updateNewsNoticias(res);
+			};
 
-    let cancel = false;
-    if (!cancel) {
-      fetchData();
-    }
-    
-    //Al desmontar
-    return ()=> {
-      cancel=true;
-      updateNewsNoticias(null);
-    }
-  }, [updateNewsNoticias])
-  
-  return (
-  <article className="BoxNoticias">
-    {list !== null ? 
-    <Noticia options={list} />
-    :
-    <Skeleton variant="rect" height={354} />}
-  </article>
-  );
+			let cancel = false;
+			if (!cancel) {
+				fetchData();
+			}
+
+			//Al desmontar
+			return () => {
+				cancel = true;
+				updateNewsNoticias(null);
+			};
+		},
+		[updateNewsNoticias]
+	);
+
+	return (
+		<article className="BoxNoticias">
+			{list !== null ? <Noticia options={list} /> : <SkeletonNoticia />}
+		</article>
+	);
 }
 
-const mapStateToProps = (state) => ({
-  list: state.news.dataN,
-})
+export function SkeletonNoticia() {
+	return (
+		<Paper variant="outlined">
+			<section className="Noticia">
+				<div className="NHead">
+					<Skeleton variant="circle" className="NHeadImg" />
+					<Skeleton variant="text" className="NHeadName" width={150} />
+				</div>
+				<hr />
+				<div className="NContent">
+					<Skeleton variant="text" className="NContentTitle" width={200} />
+					<p className="NContentP">
+						<Skeleton variant="text" width="100%" />
+						<Skeleton variant="text" width="100%" />
+						<Skeleton variant="text" width="100%" />
+						<Skeleton variant="text" width="100%" />
+					</p>
+				</div>
+				<ImagenVisor options={null} />
+			</section>
+		</Paper>
+	);
+}
+
+export function Noticia(props) {
+	const recorrerList = props.options.map(element => (
+		<div key={element.id}>
+			<Grow in={true}>
+				<Paper variant="outlined">
+					<section className="Noticia">
+						<div className="NHead">
+							<span className="NHeadImg">Logo</span>
+							<span className="NHeadName">{element.name}</span>
+						</div>
+						<hr />
+						<div className="NContent">
+							<span className="NContentTitle">{element.title}</span>
+							<p className="NContentP">{element.content}</p>
+						</div>
+						<ImagenVisor options={element.imgList} />
+					</section>
+				</Paper>
+			</Grow>
+		</div>
+	));
+
+	return recorrerList;
+}
+
+const mapStateToProps = state => ({
+	list: state.news.dataN
+});
 
 const mapDispatchToProps = {
-  updateNewsNoticias,
-}
+	updateNewsNoticias
+};
 
-
-export default connect(mapStateToProps,mapDispatchToProps)(ListNoticias);
+export default connect(mapStateToProps, mapDispatchToProps)(ListNoticias);
