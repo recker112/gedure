2773edd343f3512315d500665ef39195ff180797
Recker <joseortiz112001@gmail.com>
2020-04-16 10:25:35 -0400

    Fix text on Mantenimiento Web

--- resources/views/errors/minimal.blade.php
+++ resources/views/errors/minimal.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>@yield('title')x</title>
+        <title>@yield('title')</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
