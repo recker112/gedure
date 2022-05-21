86edc20e21724b2e48066abc0d3b22a2cda3bd53
Recker <joseortiz112001@gmail.com>
2022-05-20 22:23:20 -0400

    style: agrega user-select a todos los switchs

---
+++ frontend/src/components/form/switch/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import {
+	FormControlLabel,
+	Switch,
+} from '@mui/material';
+
+import { useController } from "react-hook-form";
+
+export function SwitchHook(props) {
+	const { 
+		label, 
+		name, 
+		control, 
+		defaultValue = false,
+		labelPlacement = 'end', 
+		...rest
+	} = props;
+	
+	const {
+    field: { ref, onChange, value, ...inputProps },
+  } = useController({
+    name,
+    control,
+    defaultValue: defaultValue,
+  });
+	
+	return (
+		<FormControlLabel
+			control={
+				<Switch
+					{...rest}
+					{...inputProps}
+					inputRef={ref}
+					onChange={e => onChange(e.currentTarget.checked)}
+					checked={value}
+				/>
+			}
+			label={label}
+			sx={{userSelect: 'none'}}
+			labelPlacement={labelPlacement}
+		/>
+	);
+}
