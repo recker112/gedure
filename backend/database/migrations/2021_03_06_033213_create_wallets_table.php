6700594a17abf9819d8b3005c8d49566ed154449
Recker <joseortiz112001@gmail.com>
2022-06-20 17:23:47 -0400

    fix: arregla duplicado de wallets

--- backend/database/migrations/2021_03_06_033213_create_wallets_table.php
+++ backend/database/migrations/2021_03_06_033213_create_wallets_table.php
@@ -15,6 +15,7 @@ class CreateWalletsTable extends Migration
 		Schema::create('wallets', function (Blueprint $table) {
 			$table->id();
 			$table->foreignId('user_id')
+			  ->unique()
 				->constrained()
 				->onUpdate('cascade')
 				->onDelete('cascade');
