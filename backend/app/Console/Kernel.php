e44ec68b01a98673ba6199c541c490564e4d98e2
Recker <joseortiz112001@gmail.com>
2022-08-05 21:17:36 -0400

    feat: mejora schedule
    - Agrega el servidor de websockets a los comandos.
    - Agrega logs a los comandos.

--- backend/app/Console/Kernel.php
+++ backend/app/Console/Kernel.php
@@ -27,20 +27,40 @@ class Kernel extends ConsoleKernel
 	{
 		// NOTA(RECKER): Reiniciar queues
 		$schedule->command('queue:restart')
-			->daily();
+			->daily()
+			->sendOutputTo(storage_path('logs/queue_restart.log'));
 		
 		// NOTA(RECKER): Iniciar queues
 		$schedule->command('queue:work --tries=3 --backoff=5 --queue=high,commands,default,emails')
-			->withoutOverlapping()
-			->runInBackground();
+		->environments(['production'])
+		->withoutOverlapping()
+		->runInBackground()
+		->appendOutputTo(storage_path('logs/queue_work.log'));
+
+		// NOTA(RECKER): Iniciar queues DEV
+		$schedule->command('queue:listen --tries=3 --backoff=5 --queue=high,commands,default,emails')
+		->environments(['local'])
+		->withoutOverlapping()
+		->runInBackground()
+		->appendOutputTo(storage_path('logs/queue_listen.log'));
+
+		// NOTA(RECKER): Iniciar websockets
+		$schedule->command('websockets:serve')
+		->environments(['production'])
+		->withoutOverlapping()
+		->runInBackground()
+		->appendOutputTo(storage_path('logs/websockets_serve.log'));
 		
 		// NOTA(RECKER): Limpiar passport
 		$schedule->command('passport:purge')
-			->daily();
+			->daily()
+			->sendOutputTo(storage_path('logs/passport_purge.log'));
 
 		// NOTA(RECKER): Actualizar precio del dolar
 		$schedule->command('exchanges:prices')
-			->hourlyAt(5);
+			->hourlyAt(5)
+			->sendOutputTo(storage_path('logs/exchanges_price.log'))
+			->runInBackground();
 		
 		/*$schedule->command('pending:payments')
 			->weeklyOn(5, '20:00');*/
