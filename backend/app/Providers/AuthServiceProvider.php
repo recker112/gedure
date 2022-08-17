adbbcdde8bb35064c543cbfd3582ef16fd9a226f
Recker <joseortiz112001@gmail.com>
2022-08-17 03:09:19 -0400

    feat: agrega env para bloquear panel websocket

--- backend/app/Providers/AuthServiceProvider.php
+++ backend/app/Providers/AuthServiceProvider.php
@@ -43,7 +43,7 @@ class AuthServiceProvider extends ServiceProvider
 		});
 
 		Gate::define('viewWebSocketsDashboard', function ($user = null) {
-			return true;
+			return env('WEBSOCKETS_URL_STATS', true);
 	});
 	}
 }
