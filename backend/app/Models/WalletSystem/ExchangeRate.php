eb030cee337c9e11499a8a144cb925c20e966585
Recker <joseortiz112001@gmail.com>
2022-11-26 02:47:24 -0400

    fix: arregla hora en exchangeRate
    - Ahora el sistema regresa la hora GMT-4 y no
    la UTC para el atributo created_at.

--- backend/app/Models/WalletSystem/ExchangeRate.php
+++ backend/app/Models/WalletSystem/ExchangeRate.php
@@ -2,6 +2,7 @@
 
 namespace App\Models\WalletSystem;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
